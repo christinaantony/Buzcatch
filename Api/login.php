96c81d2db026ab70b84eac0e0ea8437a6132090c
christina antony <82105900+christinaantony@users.noreply.github.com>
2025-02-20 15:46:27 +0530

    Update login.php

--- Api/login.php
+++ Api/login.php
@@ -2,6 +2,11 @@
 session_start();
 include '../db.php';
 header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");  
+header("Access-Control-Allow-Methods: POST"); 
+header("Access-Control-Allow-Headers: Content-Type, Access-Control - Allow-Headers, Authorization, X-Request-with"); // Allow necessary headers
+
+if($requestMethod == "POST") {
 
 $data = json_decode(file_get_contents("php://input"), true);
 $email = $data['email'];
@@ -20,5 +25,15 @@ if ($user && password_verify($password, $user['password'])) {
     echo json_encode(["success" => true, "message" => "Login successful"]);
 } else {
     echo json_encode(["error" => "Invalid email or password"]);
+}
+}
+else 
+{
+ $data = [
+    'status' => 405,
+    'message' => $requestMethod. 'Method Not Allowed',
+ ];
+ header("HTTP/1.0 405 Method Not Allowed");
+ echo json_encode($data);
 }
 ?>
